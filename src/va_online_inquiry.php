9162dee09ce8b4231e0b804cc20bbda4af730db3
AlfaRiza <malfariza45@gmail.com>
2024-10-02 20:58:59 +0700

    fix: delete variable

--- src/va_online_inquiry.php
+++ src/va_online_inquiry.php
@@ -3,15 +3,11 @@
 use BRI\Util\GetAccessToken;
 use BRI\VirtualAccount\BrivaOnline;
 
-require __DIR__ . '/../vendor/autoload.php';
-
-Dotenv\Dotenv::createUnsafeImmutable(__DIR__ . '/..' . '')->load();
-
 require __DIR__ . '/../../briapi-sdk/autoload.php';
 
 // env values
-$clientId = 'your_client_id';
-$clientSecret = 'super_secret';
+$clientId = '';
+$clientSecret = '';
 
 // url path values
 $baseUrl = 'https://api.bridex.qore.page/mock'; //base url
@@ -26,7 +22,7 @@ $accessToken = $getAccessToken->getMockOutbound(
 
 $brivaOnline = new BrivaOnline();
 
-$partnerId = 'YOWoKgXf5KcATtetyq7NbfxOz6FR65Un';
+$partnerId = '';
 
 $response = $brivaOnline->inquiry(
   $partnerId,
--- src/va_online_payment.php
+++ src/va_online_payment.php
@@ -1,17 +1,13 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
-
-Dotenv\Dotenv::createUnsafeImmutable(__DIR__ . '/..' . '')->load();
-
 require __DIR__ . '/../../briapi-sdk/autoload.php';
 
 use BRI\Util\GetAccessToken;
 use BRI\VirtualAccount\BrivaOnline;
 
 // env values
-$clientId = 'your_client_id';
-$clientSecret = 'super_secret';
+$clientId = '';
+$clientSecret = '';
 
 // url path values
 $baseUrl = 'https://api.bridex.qore.page/mock'; //base url
@@ -26,7 +22,7 @@ $accessToken = $getAccessToken->getMockOutbound(
 
 $brivaOnline = new BrivaOnline();
 
-$partnerId = 'YOWoKgXf5KcATtetyq7NbfxOz6FR65Un';
+$partnerId = '';
 
 $response = $brivaOnline->payment(
   $partnerId,
